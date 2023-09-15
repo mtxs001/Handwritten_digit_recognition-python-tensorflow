12317cde765c3aa4be7a2e7a4b78dfffdf200ffd
mtxs001 <102529146+mtxs001@users.noreply.github.com>
2023-09-15 13:45:13 +0800

    Add files via upload

---
+++ predict1.py
@@ -0,0 +1,31 @@
+"""
+作者：漫天星沙
+"""
+import tensorflow as tf
+from PIL import Image
+import numpy as np
+from train1 import CNN
+
+class Predict(object):
+    def __init__(self):
+        latest = tf.train.latest_checkpoint('./ckpt')
+        self.cnn = CNN()
+        # 恢复网络权重
+        self.cnn.model.load_weights(latest)
+    def predict(self, image_path):
+        # 以黑白方式读取图片
+        img = Image.open(image_path).convert('L')
+        img = np.reshape(img, (28, 28, 1)) / 255
+        x = np.array([1 - img])
+        # API refer: https://keras.io/models/model/
+        y = self.cnn.model.predict(x)
+        # 因为x只传入了一张图片，取y[0]即可
+        # np.argmax()取得最大值的下标，即代表的数字
+        label1=np.argmax(y[0])
+        dict1={1:'a',2:'c'}
+        print(' -> Predict digit', dict1[label1])
+if __name__ == "__main__":
+    app = Predict()
+    app.predict('./test_images/a.png')
+
+
